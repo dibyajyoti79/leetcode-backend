a59158781be40f7990bc8fbb90b163d1ec2f4953
dibyajyoti79 <dibyajyoti274@gmail.com>
2025-08-31 12:15:49 +0530

    feat: Add Mutltiple test case support

--- evaluation-service/src/config/index.ts
+++ evaluation-service/src/config/index.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 
 type ServerConfig = {
   PORT: number;
+  SUBMISSION_SERVICE: string;
 };
 
 function loadEnv() {
@@ -13,4 +14,6 @@ loadEnv();
 
 export const serverConfig: ServerConfig = {
   PORT: Number(process.env.PORT) || 8002,
+  SUBMISSION_SERVICE:
+    process.env.SUBMISSION_SERVICE || "http://localhost:8001/api/v1",
 };
